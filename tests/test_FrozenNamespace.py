a6f5693f5bbc12c8b28917b87e0356cb542ffd31
Pedro Cattori <pcattori@gmail.com>
2016-09-07 20:23:46 -0400

    fake test

--- tests/test_FrozenNamespace.py
+++ tests/test_FrozenNamespace.py
@@ -94,5 +94,8 @@ class FrozenNamespaceTest(unittest.TestCase):
     fn = FrozenNamespace(a=1, b=2)
     self.assertEqual(repr(fn), 'FrozenNamespace(a=1, b=2)')
 
+  def test_blah(self):
+    pass
+
 if __name__ == '__main__':
   unittest.main()
