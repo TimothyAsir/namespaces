bc6535e2c384d8f15625e9850bd092c93c900e45
Pedro Cattori <pcattori@gmail.com>
2016-09-08 23:15:34 -0400

    minor version bump readme and project description in setup.py altered now that default item access is restored

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
   name='namespaces',
-  version='4.0.0',
-  description='Python dictionaries with attributes instead of keys',
+  version='4.1.0',
+  description='Python dictionaries with items also accessible via dot-notation',
   url='https://github.com/pcattori/namespaces',
   author='Pedro Cattori',
   author_email='pcattori@gmail.com',
