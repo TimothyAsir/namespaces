b281de75d950ff73439bfbd318d963c9c5945131
Pedro Cattori <pcattori@gmail.com>
2016-09-08 23:44:30 -0400

    python3 supported -> minor version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='namespaces',
-  version='4.1.0',
+  version='4.2.0',
   description='Python dictionaries with items also accessible via dot-notation',
   url='https://github.com/pcattori/namespaces',
   author='Pedro Cattori',
